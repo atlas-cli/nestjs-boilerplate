62d1f09b20f7ea7fc200e5f029e05fe914d4442f
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2022-12-27 21:24:23 -0300

    pipeline: start deployment pipeline

--- scripts/diff-infra.sh
+++ scripts/diff-infra.sh
@@ -1,5 +1,7 @@
 curl -u ${CIRCLECI_TOKEN}: -X POST --header "Content-Type: application/json" -d "{
   \"parameters\": {
-    \"diff-$1\": $2
+    \"diff\": $1
   }
 }" https://circleci.com/api/v2/project/:project_slug/pipeline
+
+echo ${CIRCLECI_TOKEN}
