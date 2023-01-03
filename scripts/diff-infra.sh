6300737adb8d58cf8c8f92601360035dbc86ecc1
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2023-01-03 13:40:21 -0300

    infra: remove scripts

--- scripts/diff-infra.sh
+++
@@ -1,7 +0,0 @@
-curl -u ${CIRCLECI_TOKEN}: -X POST --header "Content-Type: application/json" -d "{
-  \"parameters\": {
-    \"diff\": $1
-  }
-}" https://circleci.com/api/v2/project/:project_slug/pipeline
-
-echo ${CIRCLECI_TOKEN}
