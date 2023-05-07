8e70ccb9c136f61a688173d029ae3c8f17e2fd0b
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2023-05-07 14:34:52 -0300

    cicd: wait on

--- test/wait-on.js
+++ test/wait-on.js
@@ -1,21 +1,41 @@
 const axios = require('axios');
 
 async function waitFor404() {
-    const url = 'http://api:3000';
+  const url = 'http://api:3000';
 
-    while (true) {
-        try {
-            await axios.get(url);
-        } catch (error) {
-            if (error.response && error.response.status === 404) {
-                console.log('API response 404');
-                break;
-            }
-        }
-
-        console.log('not response 404 try again in 1 second...');
-        await new Promise(resolve => setTimeout(resolve, 1000));
+  while (true) {
+    try {
+      await axios.get(url);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        console.log('API response 404');
+        break;
+      }
     }
+
+    console.log('not response 404 try again in 1 second...');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
 }
 
-waitFor404();
+function withTimeout(promise, timeout) {
+  let timeoutId;
+
+  const timeoutPromise = new Promise((_, reject) => {
+    timeoutId = setTimeout(() => {
+      reject(new Error(`Promise timed out after ${timeout} ms`));
+    }, timeout);
+  });
+
+  return Promise.race([promise, timeoutPromise]).finally(() => {
+    clearTimeout(timeoutId);
+  });
+}
+
+withTimeout(waitFor404(), 60000)
+  .then(() => {
+    console.log('waitFor404 completed within 60 seconds');
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
