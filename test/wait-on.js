30142b3e19922d575baab403f940677d050ba4b6
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2023-05-07 13:45:45 -0300

    cicd: wait on

--- test/wait-on.js
+++ test/wait-on.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 async function waitFor404() {
-    const url = 'http://localhost:3000';
+    const url = 'http://127.0.0.1:3000';
 
     while (true) {
         try {
