038e4468bf3cbf709ce65c847c596a1ae27d6c07
Douglas <douglasdm97@gmail.com>
2024-04-24 22:31:25 -0300

    fix: fix in unit test

--- src/auth/oidc/clients/clients.service.spec.ts
+++ src/auth/oidc/clients/clients.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientsService } from './clients.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('ClientsService', () => {
   let service: ClientsService;
@@ -7,6 +8,9 @@ describe('ClientsService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ClientsService],
+      imports: [
+        ConfigModule,
+      ],
     }).compile();
 
     service = module.get<ClientsService>(ClientsService);
